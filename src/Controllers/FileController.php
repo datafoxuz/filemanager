deedebf27d3b501129be255c21855800335fbd4e
AsadbekDev <fayzulloevasadbek@gmail.com>
2023-03-03 16:13:01 +0500

    MultiUpload

--- src/Controllers/FileController.php
+++ src/Controllers/FileController.php
@@ -3,6 +3,7 @@
 namespace Cyberbrains\Filemanager\Controllers;
 use Cyberbrains\Filemanager\UploadFileService;
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class FileController extends ApiController
@@ -23,5 +24,14 @@ class FileController extends ApiController
             return $this->sendError($exception->getMessage());
         }
         return $this->sendResponse($model, 'File successfully uploaded');
+    }
+    public function multiUpload(Request $request): JsonResponse
+    {
+        try {
+            $models = $this->fileService->multiUpload($request->file('files'));
+        } catch (Exception $exception) {
+            return $this->sendError($exception->getMessage());
+        }
+        return $this->sendResponse($models);
     }
 }
--- src/UploadFileService.php
+++ src/UploadFileService.php
@@ -2,7 +2,6 @@
 
 namespace Cyberbrains\Filemanager;
 
-use Cyberbrains\Filemanager\WebpConverter;
 use Cyberbrains\Filemanager\Models\File;
 use Exception;
 use Illuminate\Http\UploadedFile;
@@ -16,49 +15,83 @@ class UploadFileService
      * @return File|Exception
      * @throws Exception
      */
-    public function upload($file): File|Exception
+    public function upload(UploadedFile $file): File|Exception
     {
         $name = $file->getClientOriginalName();
-        $fileName = $file->getFilename();
         $user_id = Auth::id();
-        $type = strtok($file->getMimeType(), '/');
-        $path = '/storage/files/' . Auth::id() . '/';
-
+        $path = '/storage/files/' . $user_id . '/';
 
         DB::beginTransaction();
         try {
-            if ($type == 'image') {
-                $folderPath = storage_path() . '/app/public/files/' . $user_id . '/';
-                $fullPath = $folderPath . $fileName . ".webp";
-
-                if (!is_dir($folderPath)) {
-                    \Illuminate\Support\Facades\File::makeDirectory($folderPath);
-                }
-                $service = new WebpConverter();
-                $service->storePoster($file, $fullPath);
-                $link = env('STATIC_HOST') . $fileName . '.webp';
-            } else {
-                $file->store('files/' . Auth::id());
-                $link = env('STATIC_HOST') . $file->hashName();
-            }
-
-
-            $fileModel = new File();
-            $fileModel->name = $name;
-            $fileModel->ext = $file->extension();
-            $fileModel->link = $link;
-            $fileModel->user_id = Auth::id();
-            $fileModel->size = $file->getSize();
-            $fileModel->path = $path;
-            $fileModel->domain = env('STATIC_HOST');
-            $fileModel->upload_data = $file;
-            $fileModel->save();
+            $link = $this->uploadFile($file, $user_id);
+            $fileModel = $this->store($name, $file, $link, $path);
         } catch (Exception $exception) {
             DB::rollBack();
             throw $exception;
         }
         DB::commit();
+        return $fileModel;
+    }
+
+    /**
+     * @param $file
+     * @param $user_id
+     * @return string
+     */
+    function uploadFile($file, $user_id): string
+    {
+        $fileName = $file->getFilename();
+        $type = strtok($file->getMimeType(), '/');
+
+        if ($type == 'image') {
+            $folderPath = storage_path() . '/app/public/files/' . $user_id . '/';
+            $fullPath = $folderPath . $fileName . ".webp";
+
+            if (!is_dir($folderPath)) {
+                \Illuminate\Support\Facades\File::makeDirectory($folderPath);
+            }
+            $service = new WebpConverter();
+            $service->storePoster($file, $fullPath);
+            $link = env('STATIC_HOST') . $fileName . '.webp';
+        } else {
+            $file->store('files/' . Auth::id());
+            $link = env('STATIC_HOST') . $file->hashName();
+        }
+        return $link;
+    }
+
+    /**
+     * @param $name
+     * @param $file
+     * @param $link
+     * @param $path
+     * @return File
+     */
+    function store($name, $file, $link, $path): File
+    {
+        $fileModel = new File();
+        $fileModel->name = $name;
+        $fileModel->ext = $file->extension();
+        $fileModel->link = $link;
+        $fileModel->user_id = Auth::id();
+        $fileModel->size = $file->getSize();
+        $fileModel->path = $path;
+        $fileModel->domain = env('STATIC_HOST');
+        $fileModel->upload_data = $file;
+        $fileModel->save();
 
         return $fileModel;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function multiUpload($files = []): array
+    {
+        foreach ($files as $file){
+            $uploads[] = $this->upload($file);
+        }
+        return $uploads;
+
     }
 }
